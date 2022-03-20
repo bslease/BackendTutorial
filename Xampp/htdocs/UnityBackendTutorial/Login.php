91d83cbc67796c03af1c4d06f50dc3931d3044b8
bslease <bslease@gmail.com>
2022-03-20 10:21:14 -0700

    modify login.php to prevent sql injection

--- Xampp/htdocs/UnityBackendTutorial/Login.php
+++ Xampp/htdocs/UnityBackendTutorial/Login.php
@@ -11,9 +11,22 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT password, id FROM users WHERE username = '" . $loginUser . "'";
+// normal statement and query (susceptible to injection)
+// $sql = "SELECT password, id FROM users WHERE username = '" . $loginUser . "'";
+// $result = $conn->query($sql);
 
-$result = $conn->query($sql);
+// injection example
+// if user submits ' OR id = '1 as their username, the normal method above would run:
+// $sql = "SELECT password, id FROM users WHERE username = '' OR id = '1'";
+// the user could log in without knowing the username (if they know the id and the password)
+// similarly, sql injection could be used to drop entire tables, or query all sorts of info
+
+// injection soluion: use a "prepared statement" (a feature of msqli (i = "improved"))
+$sql = "SELECT password, id FROM users WHERE username = ?";
+$statement = $conn->prepare($sql);
+$statement->bind_param("s", $loginUser); // "s" means type string
+$statement->execute();
+$result = $statement->get_result();
 
 if ($result->num_rows > 0) {
   // output data of each row
